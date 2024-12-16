5508514d9166c275ba9356a8a91a532f2a0e173a
[Arpan Gupta] <arpangupta56@gmail.com>
2024-12-16 15:32:03 +0000

    Flowbite integration from online CDN to Local CDN

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -141,6 +141,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# local-cdn command not working
+
+# output for python manage.py collectstatic
+# local cdn (Content delivery network)
+STATIC_ROOTS = BASE_DIR / "local-cdn"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
