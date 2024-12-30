e79b71ccfd31f4714387113f9869a3a399b7d6f9
[Arpan Gupta] <arpangupta56@gmail.com>
2024-12-30 00:48:26 +0000

    Missing Arguments With Container

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool) #os.environ.get("DEBUG") or True #True
 
-print('DEBUG', DEBUG, type(DEBUG))
+#print('DEBUG', DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
@@ -95,7 +95,8 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL" ,cast=str)
+#DATABASE_URL = config("DATABASE_URL" ,cast=str)
+DATABASE_URL = config("DATABASE_URL" , default = None)
 
 if DATABASE_URL is not None:
     import dj_database_url
