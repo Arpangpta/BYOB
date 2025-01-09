986858658690f45302bdd091bc7bac97ccbd163d
[Arpan Gupta] <arpangupta56@gmail.com>
2025-01-09 23:05:21 +0000

    login/logout message & func. Nav bar

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,7 +21,7 @@ from .views import home_view, about_view
 from auth.views import login_view, register_view
 
 urlpatterns = [
-    path("", home_view), #index page -> root page
+    path("", home_view, name = 'home'), #index page -> root page
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
